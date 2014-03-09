2171741b1a8f431b68663219fee277026c9a6798
Olle Jonsson <olle.jonsson@gmail.com>
2014-03-09 20:08:35 +0100

    WIP: create_document

--- main.rs
+++ main.rs
@@ -103,9 +103,67 @@ impl ServerInfo {
   }
 }
 
-pub struct Database<'a> {
+pub struct Database {
   server: url::Url,
   database: ~str
+}
+
+impl Database {
+  fn do_request(&self, method: method::Method, path: ~str, data: json::Json) -> json::Json {
+    let mut url = self.server.clone();
+
+    url.path = path;
+
+    let mut request = match RequestWriter::<tcp::TcpStream>::new(method, url) {
+      Ok(req) => req,
+      Err(_) => fail!("TODO: Implement")
+    };
+
+    //request.headers.content_length = Some(data.len());
+
+    data.to_writer(&mut request);
+    
+    let mut response = match request.read_response() {
+        Ok(response) => response,
+        Err(e) => {
+          println!("{:?}", e);
+          fail!("TODO: This example can progress no further with no response :-(");
+        }
+    };
+
+    return match json::from_reader(&mut response) {
+      Ok(json) => json,
+      Err(e) => {
+        println!("{:?}", e);
+        fail!("TODO: Not a CouchDB server?");
+      }
+    };
+  }  
+  
+  pub fn create_document(&self, id: &str, content: json::Json) -> Option<(~str, ~str)> {
+    let path = format_args!(fmt::format, "/{:s}/{:s}", self.database, id);
+
+    return match self.do_request(method::Put, path, content) {
+      json::Object(tm) => {
+        match tm.find(&~"ok") {
+          Some(&json::Boolean(true)) => {
+            let id = match tm.find(&~"id") {
+              Some(&json::String(ref s)) => s.to_owned(),
+              _ => fail!("finding id")
+            };
+
+            let rev = match tm.find(&~"rev") {
+              Some(&json::String(ref s)) => s.to_owned(),
+              _ => fail!("finding rev")
+            };
+            Some((id, rev))
+          },
+          _ => fail!("finding ok")
+        }
+      },
+      _ => None
+    };
+  }
 }
 
 static server_url:&'static str = "http://localhost:5984/";
@@ -128,7 +186,10 @@ fn test_create_database() {
 fn test_create_document() {
   let couch = Couch::new(from_str(server_url).unwrap());
   let database = couch.get_database("rust");
-  let content = json::from_str("{\"magic\":true}");
-  database.create_document("test-doc-id", content);
-  assert!(true);
+  let content = json::from_str("{\"magic\":true}").unwrap();
+  assert!(match database.create_document("test-doc-id", content) {
+    Some(_) => true,
+    _ => false
+  });
+  
 }
