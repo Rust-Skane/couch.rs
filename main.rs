9fb5092c74aa827cda8be84871c7cc9d608a7b89
Jens Nockert <jens@nockert.se>
2014-03-09 18:19:01 +0100

    Fix server info up a bit

--- main.rs
+++ main.rs
@@ -1,11 +1,13 @@
 extern crate http;
 extern crate extra;
 extern crate serialize;
+extern crate collections;
 
 use std::io::net::tcp;
 
 use extra::url;
 use serialize::json;
+use collections::treemap;
 
 use http::client::RequestWriter;
 use http::method::{Get};
@@ -15,11 +17,11 @@ pub struct Couch {
 }
 
 impl Couch {
-  fn new(server: url::Url) -> Couch {
+  pub fn new(server: url::Url) -> Couch {
     return Couch { server: server };
   }
 
-  fn server_info(&self) -> json::Json {
+  pub fn server_info(&self) -> ServerInfo {
     let mut url = self.server.clone();
 
     url.path = ~"/";
@@ -34,9 +36,41 @@ impl Couch {
         Err(_) => fail!("TODO: This example can progress no further with no response :-("),
     };
 
-    return match json::from_reader(&mut response) {
+    let json = match json::from_reader(&mut response) {
       Ok(json) => json,
       Err(_) => fail!("TODO: Not a CouchDB server?")
+    };
+
+    return match json {
+      json::Object(tm) => ServerInfo { json: tm },
+      _ => fail!("TODO: Wrong format")
+    };
+  }
+}
+
+pub struct ServerInfo {
+  json: ~treemap::TreeMap<~str, json::Json>
+}
+
+impl ServerInfo {
+  pub fn couchdb<'a>(&'a self) -> &'a str {
+    return match self.json.find(&~"couchdb") {
+      Some(&json::String(ref s)) => s.as_slice(),
+      _ => fail!("Fail!")
+    }
+  }
+  
+  pub fn uuid<'a>(&'a self) -> Option<&'a str> {
+    return match self.json.find(&~"uuid") {
+      Some(&json::String(ref s)) => Some(s.as_slice()),
+      _ => None
+    }
+  }
+
+  pub fn version<'a>(&'a self) -> &'a str {
+    return match self.json.find(&~"version") {
+      Some(&json::String(ref s)) => s.as_slice(),
+      _ => fail!("Fail!")
     }
   }
 }
@@ -44,5 +78,8 @@ impl Couch {
 fn main() {
   let couch = Couch::new(from_str("http://localhost:5984/").unwrap());
 
-  println!("{:?}", couch.server_info());
+  let server_info = couch.server_info();
+  println!("{:?}", server_info.couchdb());
+  println!("{:?}", server_info.uuid());
+  println!("{:?}", server_info.version());
 }
